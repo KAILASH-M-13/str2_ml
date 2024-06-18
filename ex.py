6052be039e93cfc606493d618eb85bc8dc3186bc
KAILASH-M-13 <110119183+KAILASH-M-13@users.noreply.github.com>
2024-06-18 21:26:12 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_chat import message
 import tempfile
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
